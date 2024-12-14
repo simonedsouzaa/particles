e4c8657dc2c491f5a0576e27eb0182afafc22b30
millieorellana <millie7orellana@gmail.com>
2024-12-14 10:28:21 -0800

    Update Engine.cpp

--- Engine.cpp
+++ Engine.cpp
@@ -1,16 +1,51 @@
 #include "Engine.h"
 
-Engine::Engine(){
+Engine::Engine()
+    : m_Window(VideoMode(800, 600), "Particle Engine")
+{
+    m_particles.push_back(Particle(m_Window, 5, Vector2i(400, 300)));
 }
 
-void Engine::run(){
+void Engine::input()
+{
+    Event event;
+    while (m_Window.pollEvent(event))
+    {
+        if (event.type == Event::Closed)
+        {
+            m_Window.close();
+        }
+    }
 }
 
-Engine::input(){
+void Engine::update(float dtAsSeconds)
+{
+    for (auto& particle : m_particles)
+    {
+        particle.update(dtAsSeconds);
+    }
 }
 
-void Engine::update(float dtAsSeconds){
+void Engine::draw()
+{
+    m_Window.clear();
+    for (const auto& particle : m_particles)
+    {
+        m_Window.draw(particle);
+    }
+    m_Window.display();
 }
 
-void Engine::draw(){
+void Engine::run()
+{
+    Clock clock;
+
+    while (m_Window.isOpen())
+    {
+        float dtAsSeconds = clock.restart().asSeconds();
+
+        input();
+        update(dtAsSeconds);
+        draw();
+    }
 }
