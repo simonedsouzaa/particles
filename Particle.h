4fb08362192c665a41b260c0bd0f35dd579ddd80
millieorellana <millie7orellana@gmail.com>
2024-12-15 16:47:37 -0800

    Update Particle.h

--- Particle.h
+++ Particle.h
@@ -2,29 +2,30 @@
 #include "Matrices.h"
 #include <SFML/Graphics.hpp>
 
-#define M_PI 3.1415926535897932384626433
-const float G = 1000;      //Gravity
-const float TTL = 5.0;  //Time To Live
-const float SCALE = 0.999;
+constexpr float PI = 3.1415926535897932384626433f;
+constexpr float GRAVITY = 1000.0f;
+constexpr float TIME_TO_LIVE = 5.0f;
+constexpr float SCALE_FACTOR = 0.999f;
 
 using namespace Matrices;
 using namespace sf;
+
 class Particle : public Drawable
 {
 public:
-	Particle(RenderTarget& target, int numPoints, Vector2i mouseClickPosition);
-	virtual void draw(RenderTarget& target, RenderStates states) const override;
+    Particle(RenderTarget& target, int numPoints, Vector2i mouseClickPosition);
+    virtual void draw(RenderTarget& target, RenderStates states) const override;
     void update(float dt);
-    float getTTL() { return m_ttl; }
+    float getTTL() const noexcept { return m_ttl; }
 
-    //Functions for unit testing
-    bool almostEqual(double a, double b, double eps = 0.0001);
+    // Functions for unit testing
+    bool almostEqual(double a, double b, double eps = 0.0001) noexcept;
     void unitTests();
 
 private:
     float m_ttl;
     int m_numPoints;
-	Vector2f m_centerCoordinate;
+    Vector2f m_centerCoordinate;
     float m_radiansPerSec;
     float m_vx;
     float m_vy;
@@ -33,15 +34,7 @@ private:
     Color m_color2;
     Matrix m_A;
 
-    ///rotate Particle by theta radians counter-clockwise
-    ///construct a RotationMatrix R, left mulitply it to m_A
     void rotate(double theta);
-
-    ///Scale the size of the Particle by factor c
-    ///construct a ScalingMatrix S, left multiply it to m_A
     void scale(double c);
-
-    ///shift the Particle by (xShift, yShift) coordinates
-    ///construct a TranslationMatrix T, add it to m_A
     void translate(double xShift, double yShift);
 };
