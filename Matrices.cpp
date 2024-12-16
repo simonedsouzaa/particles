bce0a6c7a49187ca305eb8906715bb66673ebaa3
millieorellana <millie7orellana@gmail.com>
2024-12-15 19:18:23 -0800

    Update Matrices.cpp

--- Matrices.cpp
+++ Matrices.cpp
@@ -1,20 +1,18 @@
 #include "Matrices.h"
 #include <iostream>
 
-namespace Matrices
-{
+namespace Matrices {
+
     Matrix::Matrix(int _rows, int _cols)
     {
         rows = _rows;
         cols = _cols;
-        
-        a = vector<vector<double>>(rows, vector<double>(cols,0));
+        a = vector<vector<double>>(rows, vector<double>(cols, 0));
     }
-
 
     Matrix operator+(const Matrix& a, const Matrix& b)
     {
-       if (a.getRows() != b.getRows() || a.getCols() != b.getCols())
+        if (a.getRows() != b.getRows() || a.getCols() != b.getCols())
         {
             throw runtime_error("Error: dimensions must agree");
         }
@@ -25,7 +23,7 @@ namespace Matrices
         {
             for (int j = 0; j < a.getCols(); j++)
             {
-                c(i,j) = a(i,j) + b(i,j);
+                c(i, j) = a(i, j) + b(i, j);
             }
         }
         return c;
@@ -42,14 +40,14 @@ namespace Matrices
 
         for (int i = 0; i < a.getRows(); i++)
         {
-            for (int k = 0; k < b.getCols(); k++ )
+            for (int k = 0; k < b.getCols(); k++)
             {
                 double sum = 0;
                 for (int j = 0; j < a.getCols(); j++)
                 {
-                    sum += a(i,j) * b(j,k);
+                    sum += a(i, j) * b(j, k);
                 }
-                c(i,k) = sum;
+                c(i, k) = sum;
             }
         }
         return c;
@@ -65,7 +63,7 @@ namespace Matrices
         {
             for (int j = 0; j < a.getCols(); j++)
             {
-                if(abs(a(i,j) - b(i,j)) >= 0.001)
+                if (abs(a(i, j) - b(i, j)) >= 0.001)
                 {
                     return false;
                 }
@@ -76,21 +74,7 @@ namespace Matrices
 
     bool operator!=(const Matrix& a, const Matrix& b)
     {
-        if (a.getRows() != b.getRows() || a.getCols() != b.getCols())
-        {
-            return true;
-        }
-        for (int i = 0; i < a.getRows(); i++)
-        {
-            for (int j = 0; j < a.getCols(); j++)
-            {
-                if(abs(a(i,j) - b(i,j)) < 0.001)
-                {
-                    return false;
-                }
-            }
-        }
-        return true;
+        return !(a == b);
     }
 
     ostream& operator<<(ostream& os, const Matrix& a)
@@ -99,37 +83,36 @@ namespace Matrices
         {
             for (int j = 0; j < a.getCols(); j++)
             {
-                os << setw(10) << a(i,j);
-
+                os << setw(10) << a(i, j);
             }
             os << '\n';
         }
         return os;
     }
 
-    RotationMatrix::RotationMatrix(double theta) : Matrix (2,2)
+    RotationMatrix::RotationMatrix(double theta) : Matrix(2, 2)
     {
-        (*this)(0,0) = cos(theta);
-        (*this)(0,1) = -sin(theta);
-        (*this)(1,0) = sin(theta);
-        (*this)(1,1) = cos(theta);
+        (*this)(0, 0) = cos(theta);
+        (*this)(0, 1) = -sin(theta);
+        (*this)(1, 0) = sin(theta);
+        (*this)(1, 1) = cos(theta);
     }
 
-    ScalingMatrix::ScalingMatrix(double scale) : Matrix(2,2)
+    ScalingMatrix::ScalingMatrix(double scale) : Matrix(2, 2)
     {
-        (*this)(0,0) = scale;
-        (*this)(0,1) = 0;
-        (*this)(1,0) = scale;
-        (*this)(1,1) = 0;
+        (*this)(0, 0) = scale;
+        (*this)(0, 1) = 0;
+        (*this)(1, 0) = 0;  // Fix: second row, first column should be 0
+        (*this)(1, 1) = scale;
     }
 
-    TranslationMatrix::TranslationMatrix(double xShift, double yShift, int nCols) : Matrix(2,nCols)
+    TranslationMatrix::TranslationMatrix(double xShift, double yShift, int nCols) : Matrix(2, nCols)
     {
         for (int i = 0; i < nCols; i++)
         {
-            (*this)(0,i) = xShift;
-            (*this)(1,i) = yShift;
+            (*this)(0, i) = xShift;
+            (*this)(1, i) = yShift;
         }
-
     }
+
 }
